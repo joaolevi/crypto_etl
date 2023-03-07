655c5acebc6bd4b23bc77808ae568f510fdfd26b
joaolevi <joaolevi777@gmail.com>
2023-03-07 18:16:27 -0300

    Adding DB Class and CoinsModel

---
+++ src/DB/CoinsModel.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Float, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+from DBSession import DBSession
+
+Base = declarative_base()
+
+class Coins(Base):
+    __tablename__ = 'tb_coins'  # if you use base it is obligatory
+    id = Column(Integer, primary_key=True)  # obligatory
+    name = Column(String)
+    symbol = Column(String)
+    data_added = Column(Text)
+    last_updated = Column(Text)
+    price = Column(Float)
+    volume_24h = Column(Float)
+    circulating_supply = Column(Float)
+    total_supply = Column(Float)
+    max_supply = Column(Float)
+    volume_24h = Column(Float)
+    percent_change_1h = Column(Float)
+    percent_change_24h = Column(Float)
+    percent_change_7d = Column(Float)
+   
+    def start():
+        db = DBSession()
+        db.start_db_session()
+        Base.metadata.create_all(db.engine)
+        print ('\nTable created on database')
+        return db.session, db.engine
---
+++ src/DB/DBSession.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from os import path
+from inspect import getfile, currentframe
+from yaml import safe_load
+
+class DBSession():
+    def __init__(self):
+        self.session = None
+        self.engine  = None
+
+    def get_main_directory():
+        return (path.dirname(path.abspath(getfile(currentframe()))))
+
+
+    def read_config_db_file(self):
+        maindirectory = self.get_main_directory()
+        try:
+            with open(maindirectory + "config/DBConfig.yaml", 'r') as fileconfig:
+                config = safe_load(fileconfig)
+        except Exception as e:   
+            config = {'login'   : 'postgres', 
+                      'password': '1234',
+                      'ip'      : 'localhost',
+                      'port'    : '5432'}
+        return config
+    
+    def start_db_session(self):
+        config = self.read_config_db_file()
+        self.engine = create_engine('postgresql://'+ config['login'] +':'+ config['password'] +'@'+  +':'+ config['port'] +'/postgres')
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+    def save_to_db(self, data):
+        if (self.session.connection()):
+            self.session.add(data)
+            self.session.commit()
+        else:
+            self.start_db_session()
